fb16a14b1ef308470a076666be13a2924cf918d9
venkat <venkat.prabhu@10decoders.in>
2024-03-26 13:10:13 +0530

    inital-commit

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,9 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated,
+})
+export class NavComponent {}
